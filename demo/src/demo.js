bf25c8d2cde301dc37a0277b74c9276651f07657
hayden7913 <hayden321@gmail.com>
2017-02-28 08:14:22 -0800

    added snap grid

--- demo/src/demo.js
+++ demo/src/demo.js
@@ -1,23 +1,32 @@
 import React from 'react'
 import { render } from 'react-dom'
+import interact from 'interact.js'
 import Interactive from '../../src/Interactable'
 
 const draggableOptions = {
-	 onmove: event => {
-		const target = event.target
-	  // keep the dragged position in the data-x/data-y attributes
-	  const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
-	  const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+  snap: {
+     targets: [
+       interact.createSnapGrid({ x: 30, y: 30 })
+     ],
+     range: Infinity,
+     relativePoints: [ { x: 0, y: 0 } ]
+   },
+   
+   onmove: event => {
+    const target = event.target
+    // keep the dragged position in the data-x/data-y attributes
+    const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+    const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
 
-	  // translate the element
-	  target.style.webkitTransform =
-	  target.style.transform =
-	    'translate(' + x + 'px, ' + y + 'px)'
+    // translate the element
+    target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)'
 
-	  // update the posiion attributes
-	  target.setAttribute('data-x', x);
-	  target.setAttribute('data-y', y);
-	}
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  }
 }
 
 const example = (
