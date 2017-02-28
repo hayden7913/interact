e103884e38962aaa10ad7e8b52b2f63a89c4f539
hayden7913 <hayden321@gmail.com>
2017-02-28 10:54:34 -0800

    removed grid margins

--- demo/src/demo.js
+++ demo/src/demo.js
@@ -6,18 +6,20 @@ import Interactive from '../../src/Interactable'
 const draggableOptions = {
   snap: {
      targets: [
-       interact.createSnapGrid({ x: 100, y: 100 })
+       interact.createSnapGrid({ x: 10, y: 10 })
      ],
      range: Infinity,
      relativePoints: [ { x: 0, y: 0 } ]
    },
    
    onmove: event => {
+  
     const target = event.target
     // keep the dragged position in the data-x/data-y attributes
     const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
     const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
-
+    console.log(target)
+    //console.log(parseFloat(target.getAttribute('data-x')), parseFloat(target.getAttribute('data-y')))
     // translate the element
     target.style.webkitTransform =
     target.style.transform =
@@ -31,19 +33,26 @@ const draggableOptions = {
 
 const example = (
   <div>
-    <h2>Draggable</h2>
-      <Interactive draggable draggableOptions={draggableOptions}>
-        <svg>
-          <rect width="200" height="200" fill="blue" />
-        </svg>   
-      </Interactive>
+    
+    
+    
+    <svg width="100%" height="1000" >
+    <defs>
+      <pattern id="cell" width="10" height="10" patternUnits="userSpaceOnUse">
+        <path d="M 10 0 L 0 0 0 10" fill="none" stroke="#ADD8E6" strokeWidth="1"/>
+      </pattern>
+    </defs>
+    <rect width="100%" height="1000" fill="url(#cell)" />
+    <Interactive draggable draggableOptions={draggableOptions}>
+        <rect id="test-box" width="500" height="100" fill="#00a072" />
+    </Interactive>
+    <Interactive draggable draggableOptions={draggableOptions}>
+        <rect x="400" y="200" width="200" height="200" fill="#c70039" />
+    </Interactive>
+
+  </svg>
+
       
-      <Interactive draggable draggableOptions={draggableOptions}>
-        <svg>
-          <rect width="200" height="200" fill="green
-            " />
-        </svg>   
-      </Interactive>
   </div>
 )
 
